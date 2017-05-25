9d77fc2cbb674c78851f46dfc7b58278a8344bb4
Colin Davidson <colrdavidson@gmail.com>
2017-05-25 01:45:27 -0400

    Use slightly saner variable names, reuse computed variables properly

--- main.py
+++ main.py
@@ -2,37 +2,39 @@ def main():
     alpha = ['A', 'B', 'C', 'D', 'E']
 
     inputs = 3
-    bit_pad = 2**(inputs)
+    op_len = 2**(inputs)
+    total_ops = 2**(op_len)
 
     state_table = []
-    for in_state in range(bit_pad):
-        comp_str = ""
+    for in_state in range(op_len):
+        state_str = ""
         for bit_pos in range(inputs):
-            sub_bit = ((1 << bit_pos) & in_state) >> bit_pos
-            if sub_bit == 1:
-                comp_str += alpha[bit_pos]
+            state_bit = ((1 << bit_pos) & in_state) >> bit_pos
+            if state_bit == 1:
+                state_str += alpha[bit_pos]
             else:
-                comp_str += ("~" + alpha[bit_pos])
-        state_table.append(comp_str)
+                state_str += ("~" + alpha[bit_pos])
+        state_table.append(state_str)
 
     print(state_table)
 
-    for op in range(2**bit_pad):
-        comp_str = ""
+    for op in range(total_ops):
+        bitop_str = ""
         if op == 0:
-            comp_str = "nil"
-        elif op == ((2**bit_pad) - 1):
-            comp_str = "one"
+            bitop_str = "nil"
+        elif op == (total_ops - 1):
+            bitop_str = "one"
         else:
-            for bit_pos in range(bit_pad):
-                out_bit = ((1 << bit_pos) & op) >> bit_pos
-                if out_bit == 1:
-                    comp_str += state_table[len(state_table) - bit_pos - 1] + " + "
+            for bit_pos in range(op_len):
+                op_bit = ((1 << bit_pos) & op) >> bit_pos
+                if op_bit == 1:
+                    bitop_str += state_table[len(state_table) - bit_pos - 1] + " + "
 
-        if (comp_str.endswith(" + ")):
-            comp_str = comp_str[:-3]
+        if (bitop_str.endswith(" + ")):
+            bitop_str = bitop_str[:-3]
 
-        print(("{:0" + str(2**inputs) + "b}: {}").format(op, comp_str))
-    print("{} {}-ary ops".format(op+1, inputs))
+        print(("{:0" + str(op_len) + "b}: {}").format(op, bitop_str))
+
+    print("{} {}-ary ops".format(total_ops, inputs))
 
 main()
